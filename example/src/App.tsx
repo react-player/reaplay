98b3ede662ad35f7b597b5d8a5c974546584fb03
amir alipour <amir.alipour12127@gmail.com>
2021-12-15 10:12:54 +0330

    example on creating

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,13 +1,70 @@
 import React from 'react'
 
-// import { Reaplay } from 'reaplay';
+import { Reaplay } from 'reaplay';
 
-import {songs} from './Links.json'
+import { songs as songsFromUri } from './Links.json'
 
+const songsFromLocal:string[] = [
+  require('./songs/song1.mp3'),
+  require('./songs/song2.mp3'),
+  require('./songs/song3.mp3'),
+  require('./songs/song4.mp3'),
+  require('./songs/song5.mp3'),
+  require('./songs/song6.mp3'),
+]
+// you need ( require ) because react hash the file names !!!
+
+// ===============================================================
 const App = () => {
-  console.log(songs);
-  
-  return <div></div>
+  console.log(songsFromUri)
+  console.log(songsFromLocal);
+  console.log(metadata)
+
+  return (
+    <Reaplay tracks={songsFromUri} startIndex={5}>
+      {(player: any) => {
+        console.log(player);
+
+        return (
+          <div></div>
+        )
+      }}
+    </Reaplay>
+  )
 }
 
 export default App
+
+// ===============================================================
+const metadata = [
+  {
+    name: 'Song 1',
+    artist: 'artist 1',
+    album: 'album 1'
+  },
+  {
+    name: 'Song 2',
+    artist: 'artist 2',
+    album: 'album 2'
+  },
+  {
+    name: 'Song 3',
+    artist: 'artist 3',
+    album: 'album 3'
+  },
+  {
+    name: 'Song 4',
+    artist: 'artist 4',
+    album: 'album 4'
+  },
+  {
+    name: 'Song 5',
+    artist: 'artist 5',
+    album: 'album 5'
+  },
+  {
+    name: 'Song 6',
+    artist: 'artist 6',
+    album: 'album 6'
+  }
+]
